c1009076082bedd0ac65fa88a043f28ea77cfbfd
Dougal Scott <dougal.scott@gmail.com>
2025-11-15 08:22:20 +1100

    Firing

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] float bulletSpeed = 2f;
+
+    Renderer b_renderer;
+    void Start()
+    {
+        b_renderer = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        Vector2 direction = new Vector2(0f, 1f);
+        transform.Translate(bulletSpeed * Time.deltaTime * direction);
+        if (!b_renderer.isVisible)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,22 +4,41 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+
+    [SerializeField] GameObject firingPoint;
+    [SerializeField] GameObject bulletObject;
+    [SerializeField] float fireCoolDown = 0.1f;
     float rotation = 0f;
     InputAction rotateAction;
+    InputAction fireAction;
+    float lastFired;
 
     void Start()
     {
+        lastFired = Time.time;
         rotateAction = InputSystem.actions.FindAction("Move");
+        fireAction = InputSystem.actions.FindAction("Attack");
     }
 
     void Update()
     {
         DoMovement();
+        DoFiring();
     }
 
     void DoMovement()
     {
         rotation += rotateAction.ReadValue<Vector2>().x;
         transform.eulerAngles = new Vector3(0f, 0f, rotation);
+    }
+
+    void DoFiring()
+    {
+        if (fireAction.IsPressed() && Time.time > lastFired + fireCoolDown)
+        {
+            GameObject new_bullet;
+            new_bullet = Instantiate(bulletObject, firingPoint.transform.position, transform.rotation);
+            lastFired = Time.time;
+        }
     }
 }
