81e67a0956f85b971bb644fb60dc22a1bf4c2898
Dougal Scott <dougal.scott@gmail.com>
2025-11-15 12:17:35 +1100

    Asteroids wrap screen

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -1,33 +1,38 @@
+using System;
 using UnityEngine;
 
 [RequireComponent(typeof(Renderer))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class Asteroid : MonoBehaviour
 {
 
-    [SerializeField] float maxVelocity = 1f;
-    [SerializeField] float minRotation = 10f;
-    [SerializeField] float maxRotation = 20f;
+    [SerializeField] float initialMaxVelocity = 1f;
+    [SerializeField] float initialMinRotation = 10f;
+    [SerializeField] float initialMaxRotation = 20f;
 
     Vector3 velocity;
     float rotation;
     Renderer a_renderer;
-    float startTime;
+    SpriteRenderer a_sprite;
+    Camera a_camera;
 
-    void Start()
+    void Awake()
     {
         velocity = GenerateVelocity();
         rotation = GenerateRotation();
 
+
         a_renderer = GetComponent<Renderer>();
-        startTime = Time.time;
+        a_sprite = GetComponent<SpriteRenderer>();
+        a_camera = GameObject.FindAnyObjectByType<Camera>();
     }
 
     float GenerateRotation()
     // Initial Rotation Speed - nothing too slow
     {
         float new_rotation;
-        new_rotation = Random.Range(minRotation, maxRotation);
-        float rotation_dir = Random.Range(0, 1);
+        new_rotation = UnityEngine.Random.Range(initialMinRotation, initialMaxRotation);
+        float rotation_dir = UnityEngine.Random.Range(0, 1);
         if (rotation_dir == 0)
         {
             new_rotation = -new_rotation;
@@ -38,8 +43,8 @@ public class Asteroid : MonoBehaviour
     Vector3 GenerateVelocity()
     // Initial Velocity
     {
-        float vel_x = Random.Range(-maxVelocity, maxVelocity);
-        float vel_y = Random.Range(-maxVelocity, maxVelocity);
+        float vel_x = UnityEngine.Random.Range(-initialMaxVelocity, initialMaxVelocity);
+        float vel_y = UnityEngine.Random.Range(-initialMaxVelocity, initialMaxVelocity);
         return new Vector3(vel_x, vel_y, 0f);
     }
 
@@ -47,9 +52,39 @@ public class Asteroid : MonoBehaviour
     {
         transform.Rotate(new Vector3(0f, 0f, rotation * Time.deltaTime));
         transform.position += velocity * Time.deltaTime;
-        if (!a_renderer.isVisible && Time.time > startTime + 1) // First update it isn't visible for some reason
+        if (!a_renderer.isVisible)
         {
-            Destroy(gameObject);
+            CheckOffScreen();
+        }
+
+    }
+
+    void CheckOffScreen()
+    // Make asteroids wrap
+    {
+        Vector2 screen_position = a_camera.WorldToScreenPoint(transform.position);
+        float camera_z = a_camera.transform.position.z;
+
+        if (screen_position.y < 0)    // Bottom of screen
+        {
+            var top = a_camera.ScreenToWorldPoint(new Vector3(0f, Screen.height, camera_z));
+            transform.position = new Vector3(transform.position.x, top.y, 0f);
+        }
+        else if (screen_position.y > Screen.height)    // Top of screen
+        {
+            var bottom = a_camera.ScreenToWorldPoint(new Vector3(0f, 0f, camera_z));
+            transform.position = new Vector3(transform.position.x, bottom.y, 0f);
+
+        }
+        else if (screen_position.x < 0) // Left of screen
+        {
+            var right = a_camera.ScreenToWorldPoint(new Vector3(Screen.width, 0f, camera_z));
+            transform.position = new Vector3(right.x, transform.position.y, 0f);
+        }
+        else if (screen_position.x > Screen.width)    // Right of screen
+        {
+            var left = a_camera.ScreenToWorldPoint(new Vector3(0f, 0f, camera_z));
+            transform.position = new Vector3(left.x, transform.position.y, 0f);
         }
     }
 }
