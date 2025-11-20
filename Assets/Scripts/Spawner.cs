db1514ca22862cbcb5f9392247257ef7b5797f95
Dougal Scott <dougal.scott@gmail.com>
2025-11-20 11:07:34 +1100

    Continuous spawn

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,25 +1,22 @@
-using System;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
     [SerializeField] GameObject initial;
 
-    public List<Asteroid> Spawn(int numToSpawn)
+    public void SpawnAsteroids(int numToSpawn)
     {
-        List<Asteroid> spawned = new List<Asteroid>();
-        Asteroid asteroid;
         for (int i = 0; i < numToSpawn; i++)
         {
-            asteroid = Instantiate(initial).GetComponent<Asteroid>();
-            asteroid.transform.position = asteroid.GeneratePosition();
-            spawned.Add(asteroid);
+            SpawnAsteroid();
         }
-        return spawned;
     }
 
-    void Update()
+    public void SpawnAsteroid()
     {
+        Asteroid asteroid;
+
+        asteroid = Instantiate(initial).GetComponent<Asteroid>();
+        asteroid.transform.position = asteroid.GeneratePosition();
     }
 }
